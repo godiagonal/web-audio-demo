3fbed315f674ca568be8b577c6e57cfa3f05200c
godiagonal <same_johansson@hotmail.com>
2017-01-22 17:28:56 +0100

    Step 8: Restrict oscillators to musical note frequencies using a library

--- main.js
+++ main.js
@@ -11,6 +11,14 @@ gain.gain.value = 0;
 
 oscillator.start(0);
 
+setNote('A4');
+
+function setNote(note) {
+  var freq = teoria.note(note).fq();
+  oscillator.frequency.value = freq;
+  $('#frequency').text(note + ' | ' + freq.toFixed(2));
+}
+
 $('body')
   .on('mousedown', function() {
     gain.gain.value = 1;
@@ -19,9 +27,9 @@ $('body')
     gain.gain.value = 0;
   });
 
-$('body').on('mousemove', function(e) {
-  var normalizedX = e.clientX / $(e.target).width();
-  var freq = normalizedX * 1000 + 80; // Range from 80 to 1080 Hz
-  oscillator.frequency.value = freq;
-  $('#frequency').text(Math.round(freq));
-});
+// $('body').on('mousemove', function(e) {
+//   var normalizedX = e.clientX / $(e.target).width();
+//   var freq = normalizedX * 1000 + 80; // Range from 80 to 1080 Hz
+//   oscillator.frequency.value = freq;
+//   $('#frequency').text(Math.round(freq));
+// });
