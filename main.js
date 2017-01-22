ac20bff237b5de354ffe717f77628a816935335b
godiagonal <same_johansson@hotmail.com>
2017-01-22 16:55:15 +0100

    Step 2: Add oscillator and connect to destination

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 var AudioContext = window.AudioContext || window.webkitAudioContext;
 var context = new AudioContext();
 
-console.log(context.destination);
+var oscillator = context.createOscillator();
+oscillator.connect(context.destination);
+
+oscillator.start(0);
+oscillator.stop(1);
+oscillator.start(2); // Nope :(
